7de8078b03842219732fb58b07b789594887241d
Mostafa Hesham <mostafah2000@live.com>
2022-04-20 16:36:21 +0200

    Refactoring and commenting + GUI Start

--- main.py
+++ main.py
@@ -5,9 +5,9 @@
 # Initial Comment TBC
 #
 
-
 from graphviz import Digraph
 from pprint import pprint
+from tkinter import *
 
 # ----- INPUT ------
 
@@ -47,39 +47,46 @@ delta = [
 F = ['D']
 
 # preprocessing of input
-delta_dict = {}
+delta_dict = {}  # used as transition table for NFA where when given [State][Symbol] produces the next State
 
-for state in Q:
+for state in Q:  # fills the dictionary with States and Symbols only
     delta_dict[state] = {}
     for symbol in alphabet:
         delta_dict[state][symbol] = []
 
-for transition in delta:
+for transition in delta:  # fills each State and Symbol with its transitioned States
     x, s, y = transition
     delta_dict[x][s].append(y)
 
+# Print NFA
 pprint(delta_dict)
 print()
 
 
-# Algorithm
-
-def stringify(state : list):
-    return '{' + ','.join(state) + '}'
-
+# Final Graph Format Settings
 dot = Digraph()
 dot.graph_attr['rankdir'] = 'LR'
 dot.node_attr['shape'] = 'circle'
 
 
+# Algorithm NFA to DFA
 dfa_states = [[q0]]
-
 dfa_delta = []
-
 new_dfa_states = [[q0]]
 
+#                    new_dfa_states |    Symbol1 (0)   |    Symbol2 (1)   |  ...etc
+#                  -------------------------------------------------------------------
+#   current_state ->       q0       |         x        |         y        |
+#                                   |                  |                  |
+#                                   |                  |                  |
+#                                   |                  |                  |
+#
+#  x & y determined by the pre-defined dictionary as following
+#      delta_dict[current_state][symbol]   -> current_state could be more than one state like {A,B} so it's iterated
+#      upon all its elements
+
 while len(new_dfa_states) > 0:
-    current_state = new_dfa_states[0]
+    current_state = new_dfa_states[0]  #
     new_dfa_states = new_dfa_states[1:]
 
     print('Current state: ', current_state)
@@ -105,6 +112,12 @@ print()
 print('dfa_delta')
 pprint(dfa_delta)
 
+
+# construct graph by dot (Graphviz)
+def stringify(state: list):
+    return '{' + ','.join(state) + '}'
+
+
 for state in dfa_states:
     name = stringify(state)
     dot.node(name, name)
@@ -125,3 +138,14 @@ for dfa_state in dfa_states:
             dot.node(name, name, shape='doublecircle')
 
 # dot.render(filename='gv_dfa.gv', view=True)
+
+# GUI
+root = Tk()
+root.geometry(str(1000) + "x" + str(1000))
+root.title("NFA to DFA - ASU Final Automata Course Project")
+
+Label(root, text="States", font=("Montserrat", 12), fg='#666666').grid(column=1, row=0, padx=2)
+Label(root, text="Start State", font=("Montserrat", 12), fg='#666666').grid(column=2, row=0, padx=2)
+Label(root, text="Alphabet", font=("Montserrat", 12), fg='#666666').grid(column=3, row=0, padx=2)
+
+root.mainloop()
